7089510f877430ff168335b06d9e299a2b99c5db
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-10-23 10:27:31 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
         //$conn = mysqli_connect('localhost', 'root', '', '1111') or die("Can not connect database".mysqli_connect_error());
-    $Connect = pg_connect("postgres://cjltmuraqwmwns:3f6f4747c60e84cedd897375b76942235894bfa34136d489fb78e68e3121ea50@ec2-3-222-235-188.compute-1.amazonaws.com:5432/d48s61rqnmsirk");
+    $Connect = pg_connect("postgres://yjskneehcmlnxm:81b5d94761ed03ef438ccd27d59c08ae49fa6fc22edd056f6b480e924f3250a4@ec2-18-234-15-247.compute-1.amazonaws.com:5432/d59lc7bqu8ei07");
     if (!$Connect) {
         die("Connection failed");
     }
